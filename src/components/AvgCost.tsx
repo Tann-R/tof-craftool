df22337ccdfb52744ade481eda23cba1355a5ca5
Tann-R <tannilen.reagan@gmail.com>
2022-11-12 20:54:54 -0500

    I have no idea what's going on anymore.

---
+++ src/components/AvgCost.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {IResponseData, IItem, IListing} from '../app/interfaces';
+import {GetCosts} from '../remote/universalis-api';
+
+let totalValue=0;
+let totalItems=0;
+
+export function AvgCost() {
+    let itemList;
+    let cost;
+    GetCosts().then((response) => console.log(response));
+    GetCosts().then((response) => {
+        itemList=(response);
+        // It's fighting me here on mapping because ... i don't know.  it just is.
+        // try {itemList.map<any>((itemList: any) => {
+        //     itemList.forEach((gear:IItem) => {
+        //         if (gear.listings.length>0) {
+        //             totalValue+=gear.listings[0].pricePerUnit;
+        //             totalItems+=1;
+        //         }
+        //     })
+        //     cost=totalValue/totalItems;
+        //     return cost;
+        // }
+        // )}
+        // catch (error) {
+        //     console.error(error);
+        // }
+    })
+}
---
+++ src/remote/universalis-api.tsx
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+export const GetCosts = async () => {
+    await axios({
+        "method": "GET",
+        "url": "https://universalis.app/api/v2/79/37762,37763,37764,37765,37766,37767,37768,37769,37770,37771,37772,37773,37774,37775,37776,37777,37778,37779,37780,37781,37782,37783,37784,37785,37786,37787,37788,37789,37790,37791,37792,37793,37794,37795,37796,37797,37802,37807,37812,37798,37803,37808,37813,37799,37804,37809,37814,37800,37805,37810,37815,37801,37806,37811,37816?listings=1&entries=0&hq=true/"
+    })
+    .then((response:any) => {
+        return response.data.items;
+    })
+    .catch((error:any) => {
+        console.log(error)
+    })
+    }
