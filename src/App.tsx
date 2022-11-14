4757a94f15081d12e01f2e99c8c188939d9a4832
Tann-R <tannilen.reagan@gmail.com>
2022-11-14 13:32:07 -0500

    The button works. :D

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {AvgCost} from './components/AvgCost';
 
-let prices=[0,0];
-
-async function GetPricing() {
-  let [costs,setCosts]=useState([0,0]);
-  await AvgCost().then((result) => setCosts([result, (result*0.8)]));
-  prices=costs;
-}
-
 const App = () => {
-  GetPricing();
+  let [costs,setCosts]=useState([0,0]);
+  async function GetPricing() {
+    await AvgCost().then((result)=>setCosts([Math.ceil(result), Math.ceil(result*0.75)]))
+  }
   return (
     <main>
-      <div>
-        The average price per item on Rinascita gear on Cactuar is currently {prices[0]} gil.<br/>
-        With your Troupe of Fate member discount, your cost per item comes to {prices[1]} gil.
+      <div className="img1"/>
+      <div className="content">
+        <h1>Troupe of Fate Craftool</h1>
+        <h2>Calculate the average price per item for Rinascita gear on Cactuar</h2>
+        <button onClick={()=>GetPricing()} className="ToFbutton">Update Prices</button><br/>
+        <p>The average price per item on Rinascita gear on Cactuar is currently {costs[0]} gil.</p>
+        <p>With your Troupe of Fate member discount, your cost per item comes to {costs[1]} gil.</p>
+        <p>This means that, to buy a full set of Rinascita gear off the market board, you can expect to spend approximately {costs[0]*10} gil.</p>
+        <p>By purchasing your Rinascita gear from your local Troupe of Fate crafter, your projected cost for a full set of gear is {costs[1]*10} gil.</p>
+        <p>This is a savings of {(costs[0]*10)-(costs[1]*10)} gil on your gear set.</p>
+        <p>Support your Troupe of Fate crafting friends today!</p>
       </div>
+      <div className="img3"/>
     </main>
   )
 }
--- src/components/AvgCost.tsx
+++ src/components/AvgCost.tsx
@@ -1,10 +1,8 @@
-import { stringify } from 'querystring';
-import React, {useState,useEffect} from 'react';
 import { IItem } from '../app/interfaces';
 import {GetCosts} from '../remote/universalis-api';
 
 export async function AvgCost() {
-    let [price, setPrice]=useState(0);
+    let price=0
     let totalCost=0;
     let totalItems=0;
     let CalcCost=await GetCosts();
@@ -14,6 +12,6 @@ export async function AvgCost() {
             totalCost+=gear.listings[0].pricePerUnit;
             totalItems+=1;
         }});
-    setPrice(totalCost/totalItems);
+    price=(totalCost/totalItems);
     return price;
 }
